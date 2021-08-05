ad8d8ea8d063957f922ce8d35f5704bbe1edfd62
lucasgarciadev <lukas.garcia52@gmail.com>
2021-08-05 16:14:06 -0300

    add ad

--- src/Routes.js
+++ src/Routes.js
@@ -9,6 +9,7 @@ import NotFound from './pages/NotFound';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import AdPage from './pages/AdPage';
+import AddAd from './pages/AddAd';
 
 export default () => {
     return (
@@ -29,7 +30,7 @@ export default () => {
                 <AdPage/>
             </RouteHandler>
             <RouteHandler private exact path="/post-an-ad">
-                <About/>
+                <AddAd/>
             </RouteHandler>
             <RouteHandler>
                 <NotFound/>
