85b4f7993bbf126bdc502315f4a4aa5704deeffd
lucasgarciadev <lukas.garcia52@gmail.com>
2021-08-10 11:10:47 -0300

    footer

--- src/components/partials/Footer/styled.js
+++ src/components/partials/Footer/styled.js
@@ -2,5 +2,11 @@ import styled from 'styled-components';
 
 export const FooterArea = styled.div`
     height: 100px;
-    background-color: grey;
+    background-color: #CCC;
+    display:flex;
+    align-items:center;
+    justify-content:center;
+    text-align:center;
+    border-top: 1px solid #DDD;
+    margin-top: 20px;
 `;
